a9042af0ac7f2397feb0631c9bdb10944dc9cdc5
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2022-10-30 10:33:07 +0100

    Made window examples Universal apps

--- window-c/build.sh
+++ window-c/build.sh
@@ -1,4 +1,7 @@
 mkdir -p BassieTest.app/Contents/MacOS
-gcc bassietest.c -framework Cocoa -o BassieTest.app/Contents/MacOS/BassieTest || exit 1
+gcc bassietest.c -framework Cocoa -o BassieTest-arm64 || exit 1
+arch -x86_64 gcc bassietest.c -framework Cocoa -o BassieTest-x86_64 || exit 1
+lipo BassieTest-arm64 BassieTest-x86_64 -create -output BassieTest.app/Contents/MacOS/BassieTest
+rm BassieTest-arm64 BassieTest-x86_64
 cp Info.plist BassieTest.app/Contents
 open BassieTest.app
