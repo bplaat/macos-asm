6459385093976a1a135fb3bed7e11d824c223416
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2022-10-31 11:35:05 +0100

    Add release builds to the build scripts

--- window-c/build.sh
+++ window-c/build.sh
@@ -1,7 +1,12 @@
 mkdir -p BassieTest.app/Contents/MacOS
-gcc bassietest.c -framework Cocoa -o BassieTest-arm64 || exit 1
-arch -x86_64 gcc bassietest.c -framework Cocoa -o BassieTest-x86_64 || exit 1
-lipo BassieTest-arm64 BassieTest-x86_64 -create -output BassieTest.app/Contents/MacOS/BassieTest
-rm BassieTest-arm64 BassieTest-x86_64
+if [[ $1 = "release" ]]; then
+    gcc -Os bassietest.c -framework Cocoa -o BassieTest-arm64 || exit 1
+    arch -x86_64 gcc -Os bassietest.c -framework Cocoa -o BassieTest-x86_64 || exit 1
+    strip BassieTest-arm64 BassieTest-x86_64
+    lipo BassieTest-arm64 BassieTest-x86_64 -create -output BassieTest.app/Contents/MacOS/BassieTest
+    rm BassieTest-arm64 BassieTest-x86_64
+else
+    gcc bassietest.c -framework Cocoa -o BassieTest.app/Contents/MacOS/BassieTest || exit 1
+fi
 cp Info.plist BassieTest.app/Contents
 open BassieTest.app
--- window-objc/build.sh
+++ window-objc/build.sh
@@ -1,7 +1,12 @@
 mkdir -p BassieTest.app/Contents/MacOS
-gcc -x objective-c bassietest.m -framework Cocoa -o BassieTest-arm64 || exit 1
-arch -x86_64 gcc -x objective-c bassietest.m -framework Cocoa -o BassieTest-x86_64 || exit 1
-lipo BassieTest-arm64 BassieTest-x86_64 -create -output BassieTest.app/Contents/MacOS/BassieTest
-rm BassieTest-arm64 BassieTest-x86_64
+if [[ $1 = "release" ]]; then
+    gcc -Os -x objective-c bassietest.m -framework Cocoa -o BassieTest-arm64 || exit 1
+    arch -x86_64 gcc -Os -x objective-c bassietest.m -framework Cocoa -o BassieTest-x86_64 || exit 1
+    strip BassieTest-arm64 BassieTest-x86_64
+    lipo BassieTest-arm64 BassieTest-x86_64 -create -output BassieTest.app/Contents/MacOS/BassieTest
+    rm BassieTest-arm64 BassieTest-x86_64
+else
+    gcc -x objective-c bassietest.m -framework Cocoa -o BassieTest.app/Contents/MacOS/BassieTest || exit 1
+fi
 cp Info.plist BassieTest.app/Contents
 open BassieTest.app
