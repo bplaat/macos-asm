043b5ef1c6d11143596471e3f11459424f89a1b5
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2023-01-20 10:04:39 +0100

    Moved in the build scripts to the clang compiler

--- window-c/build.sh
+++ window-c/build.sh
@@ -1,12 +1,12 @@
 mkdir -p BassieTest.app/Contents/MacOS
 if [[ $1 = "release" ]]; then
-    gcc -Os bassietest.c -framework Cocoa -o BassieTest-arm64 || exit 1
-    arch -x86_64 gcc -Os bassietest.c -framework Cocoa -o BassieTest-x86_64 || exit 1
+    clang -Os bassietest.c -framework Cocoa -o BassieTest-arm64 || exit 1
+    arch -x86_64 clang -Os bassietest.c -framework Cocoa -o BassieTest-x86_64 || exit 1
     strip BassieTest-arm64 BassieTest-x86_64
     lipo BassieTest-arm64 BassieTest-x86_64 -create -output BassieTest.app/Contents/MacOS/BassieTest
     rm BassieTest-arm64 BassieTest-x86_64
 else
-    gcc bassietest.c -framework Cocoa -o BassieTest.app/Contents/MacOS/BassieTest || exit 1
+    clang bassietest.c -framework Cocoa -o BassieTest.app/Contents/MacOS/BassieTest || exit 1
 fi
 cp Info.plist BassieTest.app/Contents
 open BassieTest.app
--- window-objc/build.sh
+++ window-objc/build.sh
@@ -1,12 +1,12 @@
 mkdir -p BassieTest.app/Contents/MacOS
 if [[ $1 = "release" ]]; then
-    gcc -Os -x objective-c bassietest.m -framework Cocoa -o BassieTest-arm64 || exit 1
-    arch -x86_64 gcc -Os -x objective-c bassietest.m -framework Cocoa -o BassieTest-x86_64 || exit 1
+    clang -Os -x objective-c bassietest.m -framework Cocoa -o BassieTest-arm64 || exit 1
+    arch -x86_64 clang -Os -x objective-c bassietest.m -framework Cocoa -o BassieTest-x86_64 || exit 1
     strip BassieTest-arm64 BassieTest-x86_64
     lipo BassieTest-arm64 BassieTest-x86_64 -create -output BassieTest.app/Contents/MacOS/BassieTest
     rm BassieTest-arm64 BassieTest-x86_64
 else
-    gcc -x objective-c bassietest.m -framework Cocoa -o BassieTest.app/Contents/MacOS/BassieTest || exit 1
+    clang -x objective-c bassietest.m -framework Cocoa -o BassieTest.app/Contents/MacOS/BassieTest || exit 1
 fi
 cp Info.plist BassieTest.app/Contents
 open BassieTest.app
