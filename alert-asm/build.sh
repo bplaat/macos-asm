e64c418c2b1389bbfc7f21461851f3552491c428
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2024-08-25 18:37:24 +0200

    Update examples build shell scripts

--- alert-asm/build.sh
+++ alert-asm/build.sh
@@ -1,5 +1,7 @@
+#!/bin/sh
+set -e
 mkdir -p Alert.app/Contents/MacOS
-nasm -f bin alert.s -o Alert.app/Contents/MacOS/Alert || exit 1
+nasm -f bin alert.s -o Alert.app/Contents/MacOS/Alert
 chmod +x Alert.app/Contents/MacOS/Alert
 codesign -s - Alert.app/Contents/MacOS/Alert
 rm -rf Alert.app/Contents/_CodeSignature
--- window-objc/build.sh
+++ window-objc/build.sh
@@ -1,12 +1,14 @@
+#!/bin/sh
+set -e
 mkdir -p BassieTest.app/Contents/MacOS
 if [[ $1 = "release" ]]; then
-    clang -x objective-c --target=arm64-macos -Os bassietest.m -framework Cocoa -o BassieTest-arm64 || exit 1
-    clang -x objective-c --target=x86_64-macos -Os bassietest.m -framework Cocoa -o BassieTest-x86_64 || exit 1
+    clang -x objective-c --target=arm64-macos -Os bassietest.m -framework Cocoa -o BassieTest-arm64
+    clang -x objective-c --target=x86_64-macos -Os bassietest.m -framework Cocoa -o BassieTest-x86_64
     strip BassieTest-arm64 BassieTest-x86_64
     lipo BassieTest-arm64 BassieTest-x86_64 -create -output BassieTest.app/Contents/MacOS/BassieTest
     rm BassieTest-arm64 BassieTest-x86_64
 else
-    clang -x objective-c bassietest.m -framework Cocoa -o BassieTest.app/Contents/MacOS/BassieTest || exit 1
+    clang -x objective-c bassietest.m -framework Cocoa -o BassieTest.app/Contents/MacOS/BassieTest
 fi
 cp Info.plist BassieTest.app/Contents
 open BassieTest.app
