8abcd1226b4944a2655ee009776531b9083a8667
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2024-08-28 06:37:19 +0200

    Don't use global variables in window examples

--- window-c/bassietest.c
+++ window-c/bassietest.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <objc/runtime.h>
 #include <objc/message.h>
 
+// Cocoa headers stub
 #define cls objc_getClass
 #define sel sel_getUid
 #define msg ((id (*)(id, SEL))objc_msgSend)
 #define msg_id ((id (*)(id, SEL, id))objc_msgSend)
 #define msg_bool ((id (*)(id, SEL, BOOL))objc_msgSend)
+#define msg_int ((id (*)(id, SEL, int))objc_msgSend)
+#define msg_rect ((id (*)(id, SEL, NSRect))objc_msgSend)
+#define msg_size ((id (*)(id, SEL, NSSize))objc_msgSend)
+#define msg_rect_bool ((id (*)(id, SEL, NSRect, BOOL))objc_msgSend)
+#define msg_id_sel_id ((id (*)(id, SEL, id, SEL, id))objc_msgSend)
+#define msg_rect_int_int_int ((id (*)(id, SEL, NSRect, int, int, int))objc_msgSend)
 #define msg_cls ((id (*)(Class, SEL))objc_msgSend)
 #define msg_cls_id ((id (*)(Class, SEL, id))objc_msgSend)
+#define msg_cls_str ((id (*)(Class, SEL, char *))objc_msgSend)
+#define msg_cls_float ((id (*)(Class, SEL, CGFloat))objc_msgSend)
+#define msg_cls_float_float_float_float ((id (*)(Class, SEL, CGFloat, CGFloat, CGFloat, CGFloat))objc_msgSend)
+#define msg_ret_rect ((NSRect (*)(id, SEL))objc_msgSend)
 
 typedef double CGFloat;
 
@@ -43,24 +55,33 @@ typedef enum NSTextAlignment {
 } NSTextAlignment;
 
 id NSString(char *string) {
-    return ((id (*)(Class, SEL, char *))objc_msgSend)(cls("NSString"), sel("stringWithUTF8String:"), string);
+    return msg_cls_str(cls("NSString"), sel("stringWithUTF8String:"), string);
 }
 
-id application;
-id app_delegate;
-id window;
+// Application code
 #define LABEL_SIZE 48
-id label;
+char *app_ivar = "app";
+typedef struct App {
+    id application;
+    id appDelegate;
+    id window;
+    id label;
+} App;
 
 void windowDidResize(id self, SEL cmd) {
-    NSRect windowFrame = ((NSRect (*)(id, SEL))objc_msgSend)(window, sel("frame"));
-    ((id (*)(id, SEL, NSRect))objc_msgSend)(label, sel("setFrame:"), (NSRect){0, (windowFrame.height - LABEL_SIZE) / 2.f, windowFrame.width, LABEL_SIZE});
+    App *app;
+    object_getInstanceVariable(self, app_ivar, (void **)&app);
+    NSRect windowFrame = msg_ret_rect(app->window, sel("frame"));
+    msg_rect(app->label, sel("setFrame:"), (NSRect){0, (windowFrame.height - LABEL_SIZE) / 2.f, windowFrame.width, LABEL_SIZE});
 }
 
 void applicationDidFinishLaunching(id self, SEL cmd) {
+    App *app;
+    object_getInstanceVariable(self, app_ivar, (void **)&app);
+
     // Create menu
     id menubar = msg_cls(cls("NSMenu"), sel("new"));
-    msg_id(application, sel("setMainMenu:"), menubar);
+    msg_id(app->application, sel("setMainMenu:"), menubar);
 
     id menuBarItem = msg_cls(cls("NSMenuItem"), sel("new"));
     msg_id(menubar, sel("addItem:"), menuBarItem);
@@ -68,12 +89,12 @@ void applicationDidFinishLaunching(id self, SEL cmd) {
     id appMenu = msg_cls(cls("NSMenu"), sel("new"));
     msg_id(menuBarItem, sel("setSubmenu:"), appMenu);
 
-    id quitMenuItem = ((id (*)(id, SEL, id, SEL, id))objc_msgSend)(msg_cls(cls("NSMenuItem"), sel("alloc")),
+    id quitMenuItem = msg_id_sel_id(msg_cls(cls("NSMenuItem"), sel("alloc")),
         sel("initWithTitle:action:keyEquivalent:"), NSString("Quit BassieTest"), sel("terminate:"), NSString("q"));
     msg_id(appMenu, sel("addItem:"), quitMenuItem);
 
     // Create window
-    window = ((id (*)(id, SEL, NSRect, int, int, int))objc_msgSend)(
+    app->window = msg_rect_int_int_int(
         msg_cls(cls("NSWindow"), sel("alloc")),
         sel("initWithContentRect:styleMask:backing:defer:"),
         (NSRect){0, 0, 1024, 768},
@@ -81,31 +102,31 @@ void applicationDidFinishLaunching(id self, SEL cmd) {
         NSBackingStoreBuffered,
         NO
     );
-    msg_id(window, sel("setTitle:"), NSString("BassieTest"));
-    msg_bool(window, sel("setTitlebarAppearsTransparent:"), YES);
-    id screen = msg(window, sel("screen"));
-    NSRect screenFrame = ((NSRect (*)(id, SEL))objc_msgSend)(screen, sel("frame"));
-    NSRect windowFrame = ((NSRect (*)(id, SEL))objc_msgSend)(window, sel("frame"));
+    msg_id(app->window, sel("setTitle:"), NSString("BassieTest"));
+    msg_bool(app->window, sel("setTitlebarAppearsTransparent:"), YES);
+    id screen = msg(app->window, sel("screen"));
+    NSRect screenFrame = msg_ret_rect(screen, sel("frame"));
+    NSRect windowFrame = msg_ret_rect(app->window, sel("frame"));
     CGFloat windowX = (screenFrame.width - windowFrame.width) / 2;
     CGFloat windowY = (screenFrame.height - windowFrame.height) / 2;
-    ((id (*)(id, SEL, NSRect, BOOL))objc_msgSend)(window, sel("setFrame:display:"), (NSRect){windowX, windowY, windowFrame.width, windowFrame.height}, YES);
-    ((id (*)(id, SEL, NSSize))objc_msgSend)(window, sel("setMinSize:"), (NSSize){320, 240});
-    msg_id(window, sel("setBackgroundColor:"), ((id (*)(Class, SEL, CGFloat, CGFloat, CGFloat, CGFloat))objc_msgSend)(
+    msg_rect_bool(app->window, sel("setFrame:display:"), (NSRect){windowX, windowY, windowFrame.width, windowFrame.height}, YES);
+    msg_size(app->window, sel("setMinSize:"), (NSSize){320, 240});
+    msg_id(app->window, sel("setBackgroundColor:"), msg_cls_float_float_float_float(
         cls("NSColor"), sel("colorWithRed:green:blue:alpha:"), 0x05 / 255.f, 0x44 / 255.f, 0x5e / 255.f, 1));
-    msg_id(window, sel("setDelegate:"), app_delegate);
+    msg_id(app->window, sel("setDelegate:"), app->appDelegate);
 
     // Create label
-    label = ((id (*)(id, SEL, NSRect))objc_msgSend)(msg_cls(cls("NSText"), sel("alloc")), sel("initWithFrame:"),
+    app->label = msg_rect(msg_cls(cls("NSText"), sel("alloc")), sel("initWithFrame:"),
         (NSRect){0, (windowFrame.height - LABEL_SIZE) / 2.f, windowFrame.width, LABEL_SIZE});
-    msg_id(label, sel("setString:"), NSString("Hello macOS!"));
-    msg_id(label, sel("setFont:"), ((id (*)(Class, SEL, CGFloat))objc_msgSend)(cls("NSFont"), sel("systemFontOfSize:"), LABEL_SIZE));
-    ((id (*)(id, SEL, NSTextAlignment))objc_msgSend)(label, sel("setAlignment:"), NSTextAlignmentCenter);
-    msg_bool(label, sel("setEditable:"), NO);
-    msg_bool(label, sel("setSelectable:"), NO);
-    msg_bool(label, sel("setDrawsBackground:"), NO);
-    msg_id(msg(window, sel("contentView")), sel("addSubview:"), label);
+    msg_id(app->label, sel("setString:"), NSString("Hello macOS!"));
+    msg_id(app->label, sel("setFont:"), msg_cls_float(cls("NSFont"), sel("systemFontOfSize:"), LABEL_SIZE));
+    msg_int(app->label, sel("setAlignment:"), NSTextAlignmentCenter);
+    msg_bool(app->label, sel("setEditable:"), NO);
+    msg_bool(app->label, sel("setSelectable:"), NO);
+    msg_bool(app->label, sel("setDrawsBackground:"), NO);
+    msg_id(msg(app->window, sel("contentView")), sel("addSubview:"), app->label);
 
-    ((id (*)(id, SEL, void *))objc_msgSend)(window, sel("makeKeyAndOrderFront:"), nil);
+    msg_id(app->window, sel("makeKeyAndOrderFront:"), nil);
 }
 
 BOOL applicationShouldTerminateAfterLastWindowClosed(id self, SEL cmd) {
@@ -115,15 +136,18 @@ BOOL applicationShouldTerminateAfterLastWindowClosed(id self, SEL cmd) {
 int main(void) {
     // Register classes
     Class AppDelegate = objc_allocateClassPair(cls("NSObject"), "AppDelegate", 0);
+    class_addIvar(AppDelegate, app_ivar, sizeof(void *), log2(sizeof(void *)), "^v");
     class_addMethod(AppDelegate, sel("windowDidResize:"), (IMP)windowDidResize, "v@:");
     class_addMethod(AppDelegate, sel("applicationDidFinishLaunching:"), (IMP)applicationDidFinishLaunching, "v@:");
     class_addMethod(AppDelegate, sel("applicationShouldTerminateAfterLastWindowClosed:"), (IMP)applicationShouldTerminateAfterLastWindowClosed, "B@:");
     objc_registerClassPair(AppDelegate);
 
     // Start application
-    application = msg_cls(cls("NSApplication"), sel("sharedApplication"));
-    app_delegate = msg_cls(AppDelegate, sel("new"));
-    msg_id(application, sel("setDelegate:"), app_delegate);
-    msg(application, sel("run"));
+    App app;
+    app.application = msg_cls(cls("NSApplication"), sel("sharedApplication"));
+    app.appDelegate = msg_cls(AppDelegate, sel("new"));
+    object_setInstanceVariable(app.appDelegate, app_ivar, &app);
+    msg_id(app.application, sel("setDelegate:"), app.appDelegate);
+    msg(app.application, sel("run"));
     return EXIT_SUCCESS;
 }
--- window-objc/bassietest.m
+++ window-objc/bassietest.m
@@ -1,23 +1,28 @@
 #import <Cocoa/Cocoa.h>
 
-@interface AppDelegate : NSObject <NSApplicationDelegate, NSWindowDelegate>
-@end
+@class AppDelegate;
 
-NSApplication *application;
-NSWindow *window;
-AppDelegate *app_delegate;
 #define LABEL_SIZE 48
-NSText *label;
+typedef struct {
+    NSApplication *application;
+    NSWindow *window;
+    AppDelegate *appDelegate;
+    NSText *label;
+} App;
+
+@interface AppDelegate : NSObject <NSApplicationDelegate, NSWindowDelegate>
+    @property App *app;
+@end
 
 @implementation AppDelegate
 - (void)windowDidResize:(NSNotification *)notification {
-    label.frame = NSMakeRect(0, (NSHeight(window.frame) - LABEL_SIZE) / 2.f, NSWidth(window.frame), LABEL_SIZE);
+    _app->label.frame = NSMakeRect(0, (NSHeight(_app->window.frame) - LABEL_SIZE) / 2.f, NSWidth(_app->window.frame), LABEL_SIZE);
 }
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Create menu
     NSMenu *menubar = [NSMenu new];
-    application.mainMenu = menubar;
+    _app->application.mainMenu = menubar;
 
     NSMenuItem *menuBarItem = [NSMenuItem new];
     [menubar addItem:menuBarItem];
@@ -25,35 +30,35 @@ NSText *label;
     NSMenu *appMenu = [NSMenu new];
     menuBarItem.submenu = appMenu;
 
-    NSMenuItem* quitMenuItem = [[NSMenuItem alloc] initWithTitle:@"Quit BassieTest"
+    NSMenuItem *quitMenuItem = [[NSMenuItem alloc] initWithTitle:@"Quit BassieTest"
         action:@selector(terminate:) keyEquivalent:@"q"];
     [appMenu addItem:quitMenuItem];
 
     // Create window
-    window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 1024, 768)
+    _app->window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 1024, 768)
         styleMask:NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskMiniaturizable | NSWindowStyleMaskResizable
         backing:NSBackingStoreBuffered
         defer:NO];
-    window.title = @"BassieTest";
-    window.titlebarAppearsTransparent = YES;
-    CGFloat windowX = (NSWidth(window.screen.frame) - NSWidth(window.frame)) / 2;
-    CGFloat windowY = (NSHeight(window.screen.frame) - NSHeight(window.frame)) / 2;
-    [window setFrame:NSMakeRect(windowX, windowY, NSWidth(window.frame), NSHeight(window.frame)) display:YES];
-    window.minSize = NSMakeSize(320, 240);
-    window.backgroundColor = [NSColor colorWithRed:(0x05 / 255.f) green:(0x44 / 255.f) blue:(0x5e / 255.f) alpha:1];
-    window.delegate = app_delegate;
+    _app->window.title = @"BassieTest";
+    _app->window.titlebarAppearsTransparent = YES;
+    CGFloat windowX = (NSWidth(_app->window.screen.frame) - NSWidth(_app->window.frame)) / 2;
+    CGFloat windowY = (NSHeight(_app->window.screen.frame) - NSHeight(_app->window.frame)) / 2;
+    [_app->window setFrame:NSMakeRect(windowX, windowY, NSWidth(_app->window.frame), NSHeight(_app->window.frame)) display:YES];
+    _app->window.minSize = NSMakeSize(320, 240);
+    _app->window.backgroundColor = [NSColor colorWithRed:(0x05 / 255.f) green:(0x44 / 255.f) blue:(0x5e / 255.f) alpha:1];
+    _app->window.delegate = _app->appDelegate;
 
     // Create label
-    label = [[NSText alloc] initWithFrame:NSMakeRect(0, (NSHeight(window.frame) - LABEL_SIZE) / 2.f, NSWidth(window.frame), LABEL_SIZE)];
-    label.string = @"Hello macOS!";
-    label.font = [NSFont systemFontOfSize:LABEL_SIZE];
-    label.alignment = NSTextAlignmentCenter;
-    label.editable = NO;
-    label.selectable = NO;
-    label.drawsBackground = NO;
-    [window.contentView addSubview:label];
+    _app->label = [[NSText alloc] initWithFrame:NSMakeRect(0, (NSHeight(_app->window.frame) - LABEL_SIZE) / 2.f, NSWidth(_app->window.frame), LABEL_SIZE)];
+    _app->label.string = @"Hello macOS!";
+    _app->label.font = [NSFont systemFontOfSize:LABEL_SIZE];
+    _app->label.alignment = NSTextAlignmentCenter;
+    _app->label.editable = NO;
+    _app->label.selectable = NO;
+    _app->label.drawsBackground = NO;
+    [_app->window.contentView addSubview:_app->label];
 
-    [window makeKeyAndOrderFront:nil];
+    [_app->window makeKeyAndOrderFront:nil];
 }
 
 - (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender {
@@ -62,9 +67,11 @@ NSText *label;
 @end
 
 int main(void) {
-    application = [NSApplication sharedApplication];
-    app_delegate = [AppDelegate new];
-    application.delegate = app_delegate;
-    [application run];
+    App app;
+    app.application = [NSApplication sharedApplication];
+    app.appDelegate = [AppDelegate new];
+    app.appDelegate.app = &app;
+    app.application.delegate = app.appDelegate;
+    [app.application run];
     return EXIT_SUCCESS;
 }
