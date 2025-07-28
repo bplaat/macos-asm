437576633aeedcce5d7515b16be81287cbeb4937
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2025-07-28 09:31:41 +0200

    Remove bob dependency

--- README.md
+++ README.md
@@ -3,11 +3,5 @@
 My journey to create valid macOS MACH-O binaries in pure assembly and some other macOS and iOS programs
 
 ## Getting Started
-You need to install Xcode Command Line Tools, nasm, a Rust toolchain and [bob](https://github.com/bplaat/crates/tree/master/bin/bob) to build the examples.
 
-```sh
-xcode-select --install
-brew install nasm
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-cargo install --git https://github.com/bplaat/crates bob
-```
+You need to install Xcode Command Line Tools and `nasm` to build the examples
---
+++ window-objc/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+name=BassieTest
+CFLAGS="-x objective-c -Wall -Wextra -Werror"
+set -e
+mkdir -p $name.app/Contents/MacOS
+if [[ $1 = "release" ]]; then
+    clang $CFLAGS -Os --target=arm64-macos src/main.m -framework Cocoa -o $name-arm64
+    clang $CFLAGS -Os --target=x86_64-macos src/main.m -framework Cocoa -o $name-x86_64
+    strip $name-arm64 $name-x86_64
+    lipo $name-arm64 $name-x86_64 -create -output $name.app/Contents/MacOS/$name
+    rm $name-arm64 $name-x86_64
+else
+    clang $CFLAGS src/main.m -framework Cocoa -o $name.app/Contents/MacOS/$name
+fi
+cp Info.plist $name.app/Contents
+./$name.app/Contents/MacOS/$name
