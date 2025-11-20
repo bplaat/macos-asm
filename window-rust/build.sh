452759461c558435780e91c1d9cdbca09cfe5183
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2025-11-20 17:19:02 +0100

    Remove release building of window examples

--- window-rust/build.sh
+++ window-rust/build.sh
@@ -2,19 +2,7 @@
 name=BassieTest
 set -e
 mkdir -p $name.app/Contents/MacOS
-if [[ $1 = "release" ]]; then
-    for target in x86_64-apple-darwin aarch64-apple-darwin; do
-        cargo +nightly build --release --target $target \
-            -Z build-std=std,panic_abort \
-            -Z build-std-features=optimize_for_size \
-            -Z build-std-features=panic_immediate_abort
-        strip target/$target/release/bassietest
-    done
-    lipo target/x86_64-apple-darwin/release/bassietest target/aarch64-apple-darwin/release/bassietest \
-        -create -output $name.app/Contents/MacOS/$name
-else
-    cargo build
-    cp target/debug/bassietest $name.app/Contents/MacOS/$name
-fi
+cargo build
+cp target/debug/bassietest $name.app/Contents/MacOS/$name
 cp target/Info.plist $name.app/Contents
 ./$name.app/Contents/MacOS/$name
--- window-swift/build.sh
+++ window-swift/build.sh
@@ -2,14 +2,6 @@
 name=BassieTest
 set -e
 mkdir -p $name.app/Contents/MacOS
-if [[ $1 = "release" ]]; then
-    swiftc -Osize -target arm64-macos src/main.swift -o $name-arm64
-    swiftc -Osize -target x86_64-macos src/main.swift -o $name-x86_64
-    strip $name-arm64 $name-x86_64
-    lipo $name-arm64 $name-x86_64 -create -output $name.app/Contents/MacOS/$name
-    rm $name-arm64 $name-x86_64
-else
-    swiftc src/main.swift -o $name.app/Contents/MacOS/$name
-fi
+swiftc src/main.swift -o $name.app/Contents/MacOS/$name
 cp Info.plist $name.app/Contents
 ./$name.app/Contents/MacOS/$name
