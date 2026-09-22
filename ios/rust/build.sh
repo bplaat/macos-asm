419eefcd6db120d8a7462a0ea97b2c55de0873ae
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2026-09-22 21:26:19 +0200

    Fix Rust iOS build target and executable path

--- ios/rust/build.sh
+++ ios/rust/build.sh
@@ -4,8 +4,8 @@ name=BassieTest
 bundle_id=nl.plaatsoft.BassieTest
 
 mkdir -p "$name.app"
-cargo build --target aarch64-apple-ios-sim
-cp "target/aarch64-apple-ios-sim/debug/$name" "$name.app/$name"
+IPHONEOS_DEPLOYMENT_TARGET=15.0 cargo build --target aarch64-apple-ios-sim
+cp target/aarch64-apple-ios-sim/debug/bassietest "$name.app/$name"
 plutil -convert binary1 -o "$name.app/Info.plist" target/Info.plist
 
 xcrun simctl uninstall booted "$bundle_id"
